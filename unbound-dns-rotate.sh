61bf0b72c0ad1f1a14559b18886af343bd4e4f5b
sprokkel78 <sprokkel78.bart@gmail.com>
2025-09-02 23:23:29 +0200

    using reload instead of restart for unbound because it's way faster

--- unbound-dns-rotate.sh
+++ unbound-dns-rotate.sh
@@ -5,24 +5,24 @@ RAND=$((1 + $RANDOM % 4))
 
 if [ $RAND -eq 1 ]; then
     cp /etc/unbound/unbound.conf1 /etc/unbound/unbound.conf
-    systemctl restart unbound
+    systemctl reload unbound
 elif [ $RAND -eq 2 ]; then
     cp /etc/unbound/unbound.conf2 /etc/unbound/unbound.conf
-    systemctl restart unbound
+    systemctl reload unbound
 elif [ $RAND -eq 3 ]; then
     cp /etc/unbound/unbound.conf3 /etc/unbound/unbound.conf
-    systemctl restart unbound
+    systemctl reload unbound
 else
     cp /etc/unbound/unbound.conf4 /etc/unbound/unbound.conf
-    systemctl restart unbound
+    systemctl reload unbound
 fi
 
 # ADD ROOT-SERVER DNS BLOCK RULE
-IPT=$(iptables -C OUTPUT -p tcp --dport 53 -j DROP 2>&1 >/dev/null)
+#IPT=$(iptables -C OUTPUT -p tcp --dport 53 -j DROP 2>&1 >/dev/null)
 #echo $IPT
-if [ "$IPT" == "" ]; then
-	echo -e "root-server block rule is present"
-else
-	echo "adding root-server block rule"
-	iptables -I OUTPUT -p tcp --dport 53 -j DROP
-fi
+#if [ "$IPT" == "" ]; then
+#	echo -e "root-server block rule is present"
+#else
+#	echo "adding root-server block rule"
+#	iptables -I OUTPUT -p tcp --dport 53 -j DROP
+#fi
